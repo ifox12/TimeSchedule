c6809c92b1bc237575d1a46c8e18acc9d0745224
Tim Peschauer <tim.petschauer@gmx.net>
2019-08-14 05:44:43 +0200

    Initial

---
+++ Time/src/Time.java
@@ -0,0 +1,62 @@
+class Time {
+    private final int SECONDS_PER_HOUR = 3600;
+    private final int MINUTES_PER_HOUR = 60;
+    private final int SECONDS_PER_MINUTE = 60;
+
+    private int internalSeconds = 0;
+
+    public Time(int seconds) {
+        setSeconds(seconds);
+    }
+
+    public Time(int seconds, int minutes) {
+        setSeconds(seconds);
+        setMinutes(minutes);
+    }
+
+    public Time() {
+        setSeconds(0);
+    }
+
+
+    public Time add(int seconds) {
+        return new Time(internalSeconds + seconds);
+
+    }
+
+    public void setSeconds(int seconds) {
+        this.internalSeconds += seconds;
+    }
+
+    public void setMinutes(int minutes) {
+        this.internalSeconds += convertMinutesToInternal(minutes);
+    }
+
+    public Time add(int seconds, int minutes) {
+        return new Time(this.internalSeconds + seconds + convertMinutesToInternal(minutes));
+    }
+
+    private int convertMinutesToInternal(int minutes) {
+        return minutes * SECONDS_PER_MINUTE;
+    }
+
+    public int getSeconds() {
+        return internalSeconds % SECONDS_PER_MINUTE;
+    }
+
+    public int getMinutes() {
+        return internalSeconds / SECONDS_PER_MINUTE % MINUTES_PER_HOUR;
+    }
+
+    public int getHours() {
+        return internalSeconds / SECONDS_PER_MINUTE / MINUTES_PER_HOUR;
+    }
+
+    public Time add(int seconds, int minutes, int hours) {
+        return new Time(this.internalSeconds + seconds + convertMinutesToInternal(minutes) + convertHoursToInternal(hours));
+    }
+
+    private int convertHoursToInternal(int hours) {
+        return hours * SECONDS_PER_HOUR;
+    }
+}
---
+++ Time/tests/TimeTest.java
@@ -0,0 +1,91 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TimeTest {
+
+    @Test
+    public void add_AddSeconds_PlainAdditionResult() {
+
+        Time testTime = new Time(1);
+
+        Time result = testTime.add(2);
+
+        assertEquals(3, result.getSeconds());
+    }
+
+    @Test
+    public void add_AddSecondsCrossingOver60_ConvertFull60SecondsToMinutes() {
+
+        Time testTime = new Time(59);
+
+        Time result = testTime.add(2);
+
+        assertEquals(1, result.getMinutes());
+        assertEquals(1, result.getSeconds());
+    }
+
+    @Test
+    public void add_AddMinutesCrossingOver60_ConvertFull60MinuteToHours() {
+
+        Time testTime = new Time(0, 59);
+
+        Time result = testTime.add(0, 2);
+
+        assertEquals(1, result.getHours());
+        assertEquals(1, result.getMinutes());
+    }
+
+    @Test
+    public void add_AddMinutesAndSecondsToMinutesAndSecondsNoCrossingOver_CorrectSeperateAdditions() {
+
+        Time testTime = new Time(1, 1);
+
+        Time result = testTime.add(2, 2);
+
+        assertEquals(3, result.getMinutes());
+        assertEquals(3, result.getSeconds());
+    }
+
+    @Test
+    public void add_AddSecondsToMinutes_KeepMinutesAndJustAddSeconds() {
+
+        Time testTime = new Time(0, 1);
+
+        Time result = testTime.add(1, 0);
+
+        assertEquals(1, result.getMinutes());
+        assertEquals(1, result.getSeconds());
+    }
+
+    @Test
+    public void add_AddSecondsToMinutesCrossingOverFullMinute_CorrectAddingMinutes() {
+
+        Time testTime = new Time(59, 1);
+
+        Time result = testTime.add(2, 0);
+
+        assertEquals(1, result.getSeconds());
+        assertEquals(2, result.getMinutes());
+    }
+
+    @Test
+    public void add_AddMinutesToSecondsWithoutCrossingOver_CorrectAddition() {
+        Time testTime = new Time(1);
+
+        Time result = testTime.add(0, 1);
+
+        assertEquals(1, result.getMinutes());
+        assertEquals(1, result.getSeconds());
+    }
+
+    @Test
+    public void add_AddHoursToZero_TimeEqualsAddend() {
+        Time testTime = new Time();
+
+        Time result = testTime.add(0,0, 1);
+
+        assertEquals(0, result.getSeconds());
+        assertEquals(0, result.getMinutes());
+        assertEquals(1, result.getHours());
+    }
+}
