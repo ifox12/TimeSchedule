c7a22b89097074958078cbd25c7d6523e5efc35a
Tim Peschauer <tim.petschauer@gmx.net>
2019-08-14 05:52:23 +0200

    Make Time contents package private and sort functions

--- Time/src/Time.java
+++ Time/src/Time.java
@@ -5,55 +5,54 @@ class Time {
 
     private int internalSeconds = 0;
 
-    public Time(int seconds) {
+    Time() {
+        setSeconds(0);
+    }
+
+    Time(int seconds) {
         setSeconds(seconds);
     }
 
-    public Time(int seconds, int minutes) {
+    Time(int seconds, int minutes) {
         setSeconds(seconds);
         setMinutes(minutes);
     }
 
-    public Time() {
-        setSeconds(0);
-    }
-
-
-    public Time add(int seconds) {
+    Time add(int seconds) {
         return add(seconds, 0, 0);
 
     }
 
-    public void setSeconds(int seconds) {
+    Time add(int seconds, int minutes) {
+        return add(seconds, minutes, 0);
+    }
+
+    Time add(int seconds, int minutes, int hours) {
+        return new Time(this.internalSeconds + seconds + convertMinutesToInternal(minutes) + convertHoursToInternal(hours));
+    }
+
+    private void setSeconds(int seconds) {
         this.internalSeconds += seconds;
     }
 
-    public void setMinutes(int minutes) {
+    private void setMinutes(int minutes) {
         this.internalSeconds += convertMinutesToInternal(minutes);
     }
 
-    public Time add(int seconds, int minutes) {
-        return add(seconds, minutes, 0);
+    int getSeconds() {
+        return internalSeconds % SECONDS_PER_MINUTE;
+    }
+
+    int getMinutes() {
+        return internalSeconds / SECONDS_PER_MINUTE % MINUTES_PER_HOUR;
+    }
+
+    int getHours() {
+        return internalSeconds / SECONDS_PER_HOUR;
     }
 
     private int convertMinutesToInternal(int minutes) {
         return minutes * SECONDS_PER_MINUTE;
-    }
-
-    public int getSeconds() {
-        return internalSeconds % SECONDS_PER_MINUTE;
-    }
-
-    public int getMinutes() {
-        return internalSeconds / SECONDS_PER_MINUTE % MINUTES_PER_HOUR;
-    }
-
-    public int getHours() {
-        return internalSeconds / SECONDS_PER_HOUR;
-    }
-
-    public Time add(int seconds, int minutes, int hours) {
-        return new Time(this.internalSeconds + seconds + convertMinutesToInternal(minutes) + convertHoursToInternal(hours));
     }
 
     private int convertHoursToInternal(int hours) {
